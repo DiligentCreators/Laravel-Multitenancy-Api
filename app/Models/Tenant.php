ed1f7ec5cceb93a07bd493bc9df1d6eb27e3c02c
Shahrukh Akhtar <iam@shaz3e.com>
2026-06-07 20:10:22 +0500

    add tenancy and configration

---
+++ app/Models/Tenant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Models\Tenant as ModelsTenant;
+
+class Tenant extends ModelsTenant
+{
+    //
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,12 @@
 use App\Providers\ApiResponseServiceProvider;
 use App\Providers\AppServiceProvider;
 use App\Providers\TelescopeServiceProvider;
+use App\Providers\TenancyServiceProvider;
 
 return [
     ApiResponseServiceProvider::class,
     AppServiceProvider::class,
     TelescopeServiceProvider::class,
+    TenancyServiceProvider::class,
+
 ];
